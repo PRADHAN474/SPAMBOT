0313a715e353bf1875ae66171f92c57ac1b6f9e8
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-31 21:16:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def load_plugins(plugin_name):
-    path = Path(f"AltBots/modules/{plugin_name}.py")
-    spec = importlib.util.spec_from_file_location(f"AltBots.modules.{plugin_name}", path)
+    path = Path(f"OXYBOT/modules/{plugin_name}.py")
+    spec = importlib.util.spec_from_file_location(f"OXYBOT.modules.{plugin_name}", path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltBots.modules." + plugin_name] = load
+    sys.modules["OXYBOT.modules." + plugin_name] = load
     print("𝗢𝗫𝗬𝗚𝗘𝗡 𝗛𝗔𝗦 𝗜𝗠𝗣𝗢𝗥𝗧𝗘𝗗 " + plugin_name)
 
 
-files = glob.glob("AltBots/modules/*.py")
+files = glob.glob("OXYBOT/modules/*.py")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
