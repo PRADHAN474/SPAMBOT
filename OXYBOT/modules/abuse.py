9fd8e15ef14e5839e8d144c071e7466270bad68e
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-31 21:01:59 +0530

    Update abuse.py

--- OXYBOT/modules/abuse.py
+++ OXYBOT/modules/abuse.py
@@ -2,7 +2,7 @@ import asyncio
 from random import choice
 from telethon import events
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltBots.data import ABUSE
+from OXYBOT.data import ABUSE
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sabuse(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%sabuse(?: |$)(.*)" % hl))
