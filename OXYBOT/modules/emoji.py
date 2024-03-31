17cef92a18d67f7e6e82d5b103478c091569e404
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-31 21:05:16 +0530

    Update emoji.py

--- OXYBOT/modules/emoji.py
+++ OXYBOT/modules/emoji.py
@@ -2,7 +2,7 @@ import asyncio
 from random import choice
 from telethon import events
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltBots.data import EMOJI
+from OXYBOT.data import EMOJI
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
