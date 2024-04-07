9b0bc0122f5d83360c86be3de03f0d8faf415e24
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-04-07 12:50:14 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -35,7 +35,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/803868e39daf8729919f8.jpg",
+            "https://telegra.ph/file/b11e7d86e4622a3b3e54e.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
